bc4b0040274794bdbd07487c99c5b573c6fe1ced
Ikhlak <ikhlak789@gmail.com>
2024-11-19 16:11:12 -0800

    resolved exercise

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,18 @@
 const fs = require("fs");
 
+let output = "";
+
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+  output += data1;
+  fs.readFile("./lastname.txt", (err, data2) => {
+    output += ` ${data2}`;
+    fs.readFile("./age.txt", (err, data3) => {
+      output += ` is ${data3} years old`;
+      fs.readFile("./hobbies.txt", (err, data4) => {
+        const hobbies = JSON.parse(data4.toString()).join(" and ");
+        output += ` and his hobbies are ${hobbies}`;
+        console.log(output);
+      });
+    });
+  });
+});
